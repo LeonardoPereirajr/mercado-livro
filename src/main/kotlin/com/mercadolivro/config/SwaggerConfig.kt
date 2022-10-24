ad97c058dbebd5fc9e250bbcbc9b84666d206592
LEONARDO PEREIRA <30580018+LeonardoPereirajr@users.noreply.github.com>
2022-10-24 08:16:41 -0300

    Criação de ambientes PROD e DEV

--- src/main/kotlin/com/mercadolivro/config/SwaggerConfig.kt
+++ src/main/kotlin/com/mercadolivro/config/SwaggerConfig.kt
@@ -2,6 +2,7 @@ package com.mercadolivro.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import springfox.documentation.builders.ApiInfoBuilder
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
@@ -9,10 +10,12 @@ import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
 
+@Profile("!prod")
 @Configuration
 @EnableSwagger2
 class SwaggerConfig {
 
+    //http://localhost:8080/swagger-ui.html
     @Bean
     fun api(): Docket = Docket(DocumentationType.SWAGGER_2)
         .select()
